4161de93b32efd47e3472fdb1ad9c25d9ebc77d0
cathyatseneca <cathyatseneca@gmail.com>
2016-11-14 13:46:23 -0500

    adding traversal

--- bst.h
+++ bst.h
@@ -13,7 +13,28 @@ class BST{
 	Node* root_;	
 	//insert v into the subtree with root
 	//subtreeroot
-	void ins(v, subtreeroot);
+	void ins(int v,Node*& subtreeroot){
+		if(subtreeroot==nullptr){
+			subtreeroot=new Node(v);
+		}
+		else{
+			if(v < subtreeroot->data_){
+				ins(v,subtreeroot->left_);
+			}
+			else{
+				ins(v,subtreeroot->right_);
+			}
+		}
+	}
+	//this function prints the tree with root at address
+	//subtreeroot
+	void printInorder(const Node* subtreeroot) const {
+		if(subtreeroot!=nullptr){
+			printInorder(subtreeroot->left_);
+			cout << subtreeroot->data_ << endl;
+			printInorder(subtreeroot->right_);
+		}
+	}
 public:
 	BST(){
 		root_=nullptr;
@@ -27,18 +48,34 @@ public:
 		}
 		else{
 			Node* curr=root_;
-			while(){
-				if(data < curr->data){
-					//go left
+			bool isInserted=false;
+			while(!isInserted){
+				if(data < curr->data_){
+					if(curr->left_ != nullptr){
+						curr=curr->left_;
+					}
+					else{
+						curr->left_=new Node(v);
+						isInserted=true;						
+					}
+
 				}
 				else{
 					//go right
+					if(curr->right_ != nullptr){
+						curr=curr->right_;
+					}
+					else{
+						curr->right_=new Node(v);
+						isInserted=true;
+					}
 				}
 			}
-
 		}
 	}
 	void remove(int v);
-	void print() const;
+	void print() const{
+
+	}
 	~BST();
 }
